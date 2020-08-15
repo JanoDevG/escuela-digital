560c14b330890547a38fe40e8f7f5d3c1ab42446
Alejandro Gutiérrez <alejandro.gutierrez28@inacapmail.cl>
2020-08-14 23:07:39 -0400

    configurando las 3 rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
+  },
+  {
+    path: 'sidenav',
+    component: SidenavComponent
+  },
+  {
+    path: 'navbar',
+    component: NavbarComponent
   }
 ];
 
