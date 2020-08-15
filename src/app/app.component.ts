2b165a6c2f2efb2aa49c109cc4d580bee9f64f91
Alejandro Gutiérrez <alejandro.gutierrez28@inacapmail.cl>
2020-08-14 23:04:05 -0400

    renderizar componente con rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'AGI-root',
+
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'escuela-digital';
+  constructor(){
+    console.log("creando Objeto: AppComponent");
+  }
 }
