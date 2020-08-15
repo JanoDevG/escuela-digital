8a2aeecd90579f2b42c893ec2d43731b182e8684
Alejandro Gutiérrez <alejandro.gutierrez28@inacapmail.cl>
2020-08-14 23:52:49 -0400

    manejo de interpolación

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
-
+  titulo: string = "Lista de cursos";
+  cursos: any[] = [
+    {id: 1,
+    name: 'Angular',
+    description: 'Angular desde cero',
+    startDate: '14 de Agosto de 2020',
+    price: 30,
+    rating: 4.8,
+    imageUrl: 'assets/images/angular.svg'
+    },
+    {id: 2,
+    name: 'Node',
+    description: 'Node desde cero',
+    startDate: '14 de Agosto de 2020',
+    price: 20,
+    rating: 4.6,
+    imageUrl: 'assets/images/nodejs.svg'
+    },
+    {id: 3,
+    name: 'React',
+    description: 'React desde cero',
+    startDate: '14 de Agosto de 2020',
+    price: 30,
+    rating: 4.7,
+    imageUrl: 'assets/images/react.svg'
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
